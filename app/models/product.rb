bc716f2107443dbd56c322c073932d6fd8caa4f1
Calvin Hawkes <calvinhawkes@gmail.com>
2012-03-14 20:36:08 -0700

    added store front styles and unit testing

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ActiveRecord::Base
+    #all queries to Product by default will sort alphabetically
+    default_scope :order => 'title' 
+
+    #validation
     validates :title, :description, :image_url, :presence => true
     validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
     validates :title, :uniqueness => true
